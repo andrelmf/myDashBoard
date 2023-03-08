2b0c0b643a61ba9e046de3551e77d53d3447fa8b
André Ferreira <andrelmf961@gmail.com>
2023-03-08 13:03:49 -0300

    teste

--- src/main/java/com/app/crud/crud_springboot/controlador/UsuarioControlador.java
+++ src/main/java/com/app/crud/crud_springboot/controlador/UsuarioControlador.java
@@ -20,7 +20,7 @@ public class UsuarioControlador {
     private UsuarioServicoInterface servico;
 
     // Endereço da Home page
-    @GetMapping({"/home", "/"})
+    @GetMapping({"/", "/home"})
     public String goToHomePage(){
         return "home";
     }
